cace90572029757922d101f32994ddf7fb9e0ebd
Shivasankar Gunasekaran <shivasankarg.g@gmail.com>
2017-04-25 02:24:06 -0600

    added TODO items in DB schema

--- DBSchema.sql
+++ DBSchema.sql
@@ -21,3 +21,57 @@ CREATE TABLE `User_tbl` (
   KEY `userRoleId` (`userRoleId`),
   CONSTRAINT `user_tbl_ibfk_1` FOREIGN KEY (`userRoleId`) REFERENCES `UserRole_tbl` (`userRoleId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+# A BIG TODO:
+# Finish this
+# Get it verified
+# And then Run
+# (Press CTRL + / for the following)
+-- CREATE TABLE `Movie_tbl` (
+--   `movieId`
+--   `movieName`
+--   `releaseYear`
+--   `genre`
+--   `aggregateRating`
+--   `numberOfUsersRated`
+--   `numberOfCriticsRated`
+--   `approvalState`
+-- 
+-- 
+--   `userRoleId` int(11) DEFAULT NULL,
+--   `name` varchar(100) NOT NULL,
+--   `password` varchar(100) DEFAULT NULL,
+--   `username` varchar(100) NOT NULL,
+--   `emailAddress` varchar(100) DEFAULT NULL,
+--   PRIMARY KEY (`userId`),
+--   KEY `userRoleId` (`userRoleId`),
+--   CONSTRAINT `user_tbl_ibfk_1` FOREIGN KEY (`userRoleId`) REFERENCES `UserRole_tbl` (`userRoleId`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+-- 
+
+-- CREATE TABLE `Review_tbl` (
+-- 	
+-- 	`reviewId`
+--     `rating`
+--     `comment`
+--     `flag`
+--     `userId`
+--     `movieId`
+-- 	
+-- 
+--   `userId` int(11) NOT NULL AUTO_INCREMENT,
+--   `userRoleId` int(11) DEFAULT NULL,
+--   `name` varchar(100) NOT NULL,
+--   `password` varchar(100) DEFAULT NULL,
+--   `username` varchar(100) NOT NULL,
+--   `emailAddress` varchar(100) DEFAULT NULL,
+--   PRIMARY KEY (`userId`),
+--   KEY `userRoleId` (`userRoleId`),
+--   CONSTRAINT `user_tbl_ibfk_1` FOREIGN KEY (`userRoleId`) REFERENCES `UserRole_tbl` (`userRoleId`)
+-- ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+-- 
+
+
+# What about another table for notifications?
+# How are we going to implement that?
+
