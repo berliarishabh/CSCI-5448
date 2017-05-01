8ded4e06270836081cfb3f57b4915cf95ece9a58
Omkar Reddy Seelam <omkar.reddy15@gmail.com>
2017-04-30 22:40:10 -0600

    cleaned up add review usecase code

--- MovieRatingSystem/src/main/java/MRS/controllers/ReviewController.java
+++ MovieRatingSystem/src/main/java/MRS/controllers/ReviewController.java
@@ -2,6 +2,7 @@ package MRS.controllers;
 
 import MRS.Model.Review;
 import MRS.Common.*;
+import MRS.controllers.LoginController;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,41 +11,39 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/review")
 public class ReviewController {
 
-	private String ratingStr = "89";
-	private String userIdStr = "2";
-	private String movieIdStr= "4";
-	private String comment	 = "This is a test comment";
-	
 	private DBProxy dbProxy;
 	private ReviewController()
 	{
 		dbProxy = new DBProxy();
-		this.review();
 	}
-	
-	//@PostMapping("/review")
-//	public void review(Model model, @RequestParam String ratingStr, @RequestParam String comment, 
-//			@RequestParam String userIdStr, @RequestParam String movieIdStr) {
 
-	public void review() {
-		
-		double rating 	= Double.parseDouble(ratingStr);
-		int userId	 	= Integer.parseInt(userIdStr);
-		int movieId		= Integer.parseInt(movieIdStr);
-		
-		Review newReview = new Review(rating, comment, userId, movieId);
+	@PostMapping("/review")
+	public String review(Model model, @RequestParam String ratingStr, @RequestParam String comment, 
+			@RequestParam String userIdStr, @RequestParam String movieIdStr) {
 
-		boolean addReviewReturnCode = dbProxy.addReview(newReview);
+		String retval = "joinus";	// return to the login page
+
+		if (isLoggedIn() == true) {
+			double rating 	= Double.parseDouble(ratingStr);
+			int userId	 	= Integer.parseInt(userIdStr);
+			int movieId		= Integer.parseInt(movieIdStr);
+			Review newReview = new Review(rating, comment, userId, movieId);
+			
+			boolean addReviewReturnCode = dbProxy.addReview(newReview);
+			
+			if (addReviewReturnCode == true) {
+				System.out.println("\nAdding review successful");
+			}
+			else {
+				System.out.println("\nAdding review failed");
+			}
+
+			retval = "reviews";		// reload the page
+		}
 		
-		if (addReviewReturnCode == true) {
-			System.out.println("\nAdding review successful");
-		}
-		else {
-			System.out.println("\nAdding review failed");
-		}
-	}
-	
+		return retval;
+	}	
 }
+
