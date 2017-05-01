798948c7029bc7f8718e6fac957b2a7c602b2106
berliarishabh <rishabh.berlia@colorado.edu>
2017-04-30 22:19:31 -0600

    UI Web Templates with JSON Updated

--- UI_WebView_HTML/backup_old/js/app.js
+++ UI_WebView_HTML/backup_old/js/app.js
@@ -1,11 +1,11 @@
 (function($, document, window){
-
+	
 	$(document).ready(function(){
 
 		// Cloning main navigation for mobile menu
 		$(".mobile-navigation").append($(".main-navigation .menu").clone());
 
-		// Mobile menu toggle
+		// Mobile menu toggle 
 		$(".menu-toggle").click(function(){
 			$(".mobile-navigation").slideToggle();
 		});
@@ -26,15 +26,15 @@
 			$('.map').gmap3({
 				map: {
 					options: {
-						maxZoom: 14
-					}
+						maxZoom: 14 
+					}  
 				},
 				marker:{
 					address: "40 Sibley St, Detroit",
 				}
 			},
 			"autofit" );
-
+	    	
 	    }
 	});
 
